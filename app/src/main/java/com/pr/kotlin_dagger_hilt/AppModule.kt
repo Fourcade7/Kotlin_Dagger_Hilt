992cfee441bddd1f22824588ba9bbb63df948bf6
Fourcade7 <fourcadeaminov7@gmail.com>
2022-09-16 09:51:27 +0500

    Initial commit

---
+++ app/src/main/java/com/pr/kotlin_dagger_hilt/AppModule.kt
@@ -0,0 +1,17 @@
+package com.pr.kotlin_dagger_hilt
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+class AppModule {
+
+    @Singleton
+    @Provides
+    fun provideTestString()="This is a string will inject777"
+}
---
+++ app/src/main/java/com/pr/kotlin_dagger_hilt/MyApplication.kt
@@ -0,0 +1,7 @@
+package com.pr.kotlin_dagger_hilt
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MyApplication:Application()
